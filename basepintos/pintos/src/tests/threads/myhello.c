2ad1cda7b3ab7c17cf74919095a526f88878f4ea
Archies09 <adfzr007@gmail.com>
2018-01-22 00:47:23 +0530

    Pintos Threads Project Complete

---
+++ basepintos/pintos/src/tests/threads/myhello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/init.h"
+#include "devices/timer.h"
+#include "threads/malloc.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+
+void
+my_hello (void)
+{
+	printf("Hello Pintos!!!!\n");
+}
